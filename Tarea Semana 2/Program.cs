07f92dc4a23f94a9fcb9ad47bff8b91ce46300e9
JoseFabianTeoSandoval <jteos1@miumg.edu.gt>
2024-02-16 05:06:23 -0600

    aaaaaaaaaaaaaaaaaaaaaaaa

--- Tarea Semana 2/Program.cs
+++ Tarea Semana 2/Program.cs
@@ -1,15 +1,1 @@
-﻿using System;
-
-namespace entero_a_decimal
-int numE;
-decimal numD;
-Console.WriteLine("ingrese un numero entero");
-numE = Convert.ToInt32(Console.ReadLine())
-numD = Convert.ToDecimal(numE)
-Console.WriteLine("entero a decimal=" + numD)
-
-namespace Tarea_Semana_2
-int num1;
-int num2;
-Console.WriteLine("ingrese un numero")
-
+﻿
