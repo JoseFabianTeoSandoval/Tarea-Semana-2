1330b0be53d3455596cb1b97f32dbdc8fa08918c
JoseFabianTeoSandoval <jteos1@miumg.edu.gt>
2024-02-16 16:39:16 -0600

    Version Final

--- Tarea Semana 2/Program.cs
+++ Tarea Semana 2/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Linq;
 
-namespace ConversionDeTipos
+namespace TareaSemana2
 {
     class Program
     {
@@ -37,41 +37,41 @@ namespace ConversionDeTipos
             Console.Write("\nOperacion con tipos de datos");
 
             Console.WriteLine("\nSuma de dos Enteros");
-            Console.Write("Ingrese el primer número entero:");
+            Console.Write("Ingrese el primer número entero: ");
             int num1 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Ingrese el segundo número entero:");
+            Console.Write("Ingrese el segundo número entero: ");
             int num2 = Convert.ToInt32(Console.ReadLine());
             int suma = num1 + num2;
             Console.WriteLine("La suma de {0} y {1} es: {2}", num1, num2, suma);
 
             Console.WriteLine("\nResta de dos Decimales");
-            Console.Write("Ingrese el primer número decimal:");
+            Console.Write("Ingrese el primer número decimal: ");
             decimal num3 = Convert.ToDecimal(Console.ReadLine());
-            Console.Write("Ingrese el segundo número decimal:");
+            Console.Write("Ingrese el segundo número decimal: ");
             decimal num4 = Convert.ToDecimal(Console.ReadLine());
             decimal resta = num3 - num4;
             Console.WriteLine("La resta de {0} y {1} es: {2}", num3, num4, resta);
 
             Console.WriteLine("\nMultiplicacion de dos numeros flotantes");
-            Console.Write("Ingrese el primer número flotante:");
+            Console.Write("Ingrese el primer número flotante: ");
             float num5 = Convert.ToSingle(Console.ReadLine());
-            Console.Write("Ingrese el segundo número flotante:");
+            Console.Write("Ingrese el segundo número flotante: ");
             float num6 = Convert.ToSingle(Console.ReadLine());
             float multiplicacion = num5 * num6;
             Console.WriteLine("La multiplicación de {0} y {1} es: {2}", num5, num6, multiplicacion);
 
             Console.WriteLine("\nDivision entre dos enteros");
-            Console.Write("Ingrese el numerador (número entero):");
+            Console.Write("Ingrese el numerador: ");
             int num7 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Ingrese el denominador (número entero distinto de cero):");
+            Console.Write("Ingrese el denominador: ");
             int num8 = Convert.ToInt32(Console.ReadLine());
             double division = (double)num7 / num8;
             Console.WriteLine("La división de {0} entre {1} es: {2}", num7, num8, division);
 
             Console.WriteLine("\nModulo de dos enteros");
-            Console.Write("Ingrese el primer número entero:");
+            Console.Write("Ingrese el primer número entero: ");
             int num9 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Ingrese el segundo número entero:");
+            Console.Write("Ingrese el segundo número entero: ");
             int num10 = Convert.ToInt32(Console.ReadLine());
             int modulo = num9 % num10;
             Console.WriteLine("El módulo de {0} y {1} es: {2}", num9, num10, modulo);
@@ -100,6 +100,43 @@ namespace ConversionDeTipos
             int contador = cadena.Length - vocales.Length;
             Console.WriteLine($"El número de vocales en la cadena de texto es: {contador}");
 
+            Console.Write("\nUso de tipos de datos numericos");
+
+            Console.WriteLine("\narea de un triangulo");
+            Console.Write("Ingrese la base del triángulo: ");
+            double baseT = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese la altura del triángulo: ");
+            double altura = Convert.ToDouble(Console.ReadLine());
+            double area = (baseT * altura) / 2;
+            Console.WriteLine($"El área del triángulo es: {area}");
+
+            Console.WriteLine("\nvolumen de una esfera");
+            Console.Write("Ingrese el radio de la esfera: ");
+            double radio = Convert.ToDouble(Console.ReadLine());
+            double volumen = (4.0 / 3.0) * Math.PI * Math.Pow(radio, 3);
+            Console.WriteLine($"El volumen de la esfera es: {volumen}");
+
+            Console.WriteLine("\nDistancia entre puntos");
+            Console.Write("Ingrese las coordenadas del primer punto (x1, y1): ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+            double y1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Ingrese las coordenadas del segundo punto (x2, y2): ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+            double y2 = Convert.ToDouble(Console.ReadLine());
+            double distancia = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine($"La distancia entre los dos puntos es: {distancia}");
+
+            Console.WriteLine("\nCelsius a Fahrenheit");
+            Console.Write("Ingrese la temperatura en grados Celsius: ");
+            double celsius = Convert.ToDouble(Console.ReadLine());
+            double fahrenheit = (celsius * 9 / 5) + 32;
+            Console.WriteLine($"La temperatura en grados Fahrenheit es: {fahrenheit}");
+
+            Console.WriteLine("\nFahrenheit a Celsius");
+            Console.Write("Ingrese la temperatura en grados Fahrenheit: ");
+            double fahrenheit2 = Convert.ToDouble(Console.ReadLine());
+            double celsius2 = (fahrenheit2 - 32) * 5 / 9;
+            Console.WriteLine($"La temperatura en grados Celsius es: {celsius2}");
 
             Console.ReadLine();
         }
